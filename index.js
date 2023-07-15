a42561a95baf7341917fd2fcc48946e8cbe50332
dhuziran <danielhuziran@gmail.com>
2023-07-14 20:41:00 -0500

    first commit

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const PORT = 8080;
 // GET - / - returns homepage
 app.get('/', (req, res) => {
     // serve up the public folder as static index.html file
-
 });
 
 // hello world route
@@ -21,16 +20,23 @@ app.get('/api', (req, res) => {
 // get all pets from the database
 app.get('/api/v1/pets', (req, res) => {
     // send the pets array as a response
+    console.log(pets)
+    res.send(pets)
 
 });
 
 // get pet by name
 app.get('/api/v1/pets/:name', (req, res) => {
     // get the name from the request
+    const singlePet = pets.find((e) => {
+        if(e.name == req.params.name){
+            return true
+        }
+    })
+    res.send(singlePet)
 
 
     // find the pet in the pets array
-    const pet = pets.find(pet => pet.name === name);
 
     // send the pet as a response
 
@@ -39,12 +45,17 @@ app.get('/api/v1/pets/:name', (req, res) => {
 // get pet by owner with query string
 app.get('/api/v1/pets/owner', (req, res) => {
     // get the owner from the request
-
+    const owner = req.query.owner
 
     // find the pet in the pets array
-    const pet = pets.find(pet => pet.owner === owner);
+    const pet = pets.find((e) => {
+        if(e.owner == owner){
+            return true
+        }
+    });
 
     // send the pet as a response
+    res.send(pet)
 
 });
 
